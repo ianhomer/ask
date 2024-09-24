bbb2657d70d69707d805bed3cb19789cc56438ba
Ian Homer <code@ianhomer.com>
2024-09-24 10:58:49 +0100

    ✅ Only remove API KEY from test environment if already exists

--- ask/tests/conftest.py
+++ ask/tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 from ..gemini import API_KEY_NAME
 
-os.environ.pop(API_KEY_NAME)
+# Safety check to ensure that API_KEY is not passed into unit tests since this
+# could have unintended side effect from environment leaking in
+if API_KEY_NAME in os.environ:
+    os.environ.pop(API_KEY_NAME)
 
 assert API_KEY_NAME not in os.environ
