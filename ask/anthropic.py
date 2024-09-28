9201b07abd32341b8f43e9a5dcbd5a81b8f0e88a
Ian Homer <code@ianhomer.com>
2024-09-28 18:53:40 +0100

    ✨ Anthropic API support

--- ask/anthropic.py
+++ ask/anthropic.py
@@ -1,6 +1,8 @@
 import os
 from anthropic import Anthropic
 from typing import Optional
+
+from anthropic.types import TextBlock
 from .bot_service import BotService
 from .renderer import AbstractRenderer
 
@@ -32,19 +34,21 @@ class AnthropicService(BotService):
     def available(self) -> bool:
         return self._available
 
-    def process(self, user_input: Optional[str]) -> Optional[str]:
+    def process(self, user_input: str) -> Optional[str]:
         try:
             message = self.client.messages.create(
-                max_tokens=1024,
+                max_tokens=4906,
                 messages=[
                     {
                         "role": "user",
-                        "content": "Hello, Claude",
+                        "content": user_input,
                     }
                 ],
                 model="claude-3-5-sonnet-20240620",
             )
-            return message.content
+            content = message.content[0]
+            if type(content) is TextBlock:
+                return content.text
         except Exception as e:
             print(f"\nCannot process prompt \n{user_input}\n", e)
 
